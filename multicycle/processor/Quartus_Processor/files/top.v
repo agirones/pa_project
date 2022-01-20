ffb8fc7a2234a9fa642fc6737db538c4ffaa3513
RogerGN <rogarcianevado@gmail.com>
2022-01-20 03:13:32 +0100

    + added quartus 20.1 project (current compiler is SystemVerilog, otherwise input/output logic variables have to be defined into reg/wire)

---
+++ multicycle/processor/Quartus_Processor/files/top.v
@@ -0,0 +1,11 @@
+module top(input clk, reset,
+           output [31:0] WriteData, ALUOut,
+           output MemWrite);
+
+wire [31:0] instr, ReadData, pc;
+
+riscv riscv(clk, reset, instr, ReadData, pc, ALUOut, WriteData, MemWrite);
+dmem dmem(clk, MemWrite, ALUOut, WriteData, ReadData);
+imem imem(pc, instr);
+
+endmodule
