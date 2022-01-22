8bae29c666c7c49bb4bf728b478fcaf915ab48d6
RogerGN <rogarcianevado@gmail.com>
2022-01-22 02:42:49 +0100

    +signals

--- multicycle/processor/maindec.v
+++ multicycle/processor/maindec.v
@@ -3,43 +3,51 @@
 module maindec(input clk, reset, ihit, dhit,
                input [6:0] opcode,
                input [2:0] funct,
-               output RegWriteW, MemWriteM, LoadD, ByteD, ALUSrcE, ByteW, MemtoRegW,
+               output RegWriteW, MemWriteM, LoadD, BranchD, JumpD, ByteD, ALUSrcE, ByteW, MemtoRegW,
                output [1:0] aluop);
 
-reg [7:0] controls;
-wire RegWriteF, MemWriteF, LoadF, ByteF, ALUSrcF, MemtoRegF;
-wire RegWriteD, MemWriteD, ByteD, ALUSrcD, MemtoRegD;
-wire RegWriteE, MemWriteE, ByteE, MemtoRegE;
+reg [9:0] controls;
+wire RegWriteF, MemWriteF, LoadF, BranchF, JumpF, ByteF, ALUSrcF, MemtoRegF;
+wire RegWriteD, MemWriteD, LoadD, BranchD, JumpD, ByteD, ALUSrcD, MemtoRegD;
+wire RegWriteE, MemWriteE, LoadE, ByteE, MemtoRegE;
 wire RegWriteM, ByteM, MemtoRegM;
 
-assign {RegWriteF, MemWriteF, LoadF, ByteF, ALUSrcF, MemtoRegF, aluop} = controls;
+assign {RegWriteF, MemWriteF, LoadF, BranchD, JumpD, ByteF, ALUSrcF, MemtoRegF, aluop} = controls;
 
 always @(*)
     if(~reset)
 	    if (ihit)
 		case(opcode)
 		    7'b0000011: case(funct)
-				    3'b000:  controls <= 8'b10111100; //LB
-				    3'b010:  controls <= 8'b10101100; //LW
-				    default: controls <= 8'bxxxxxxxx;
+				    3'b000:  controls <= 10'b1010011100; //LB
+				    3'b010:  controls <= 10'b1010001100; //LW
+				    default: controls <= 10'bxxxxxxxxxx;
 				endcase
 		    7'b0100011: case(funct)
-				    3'b000:  controls <= 8'b01011x00; //SB
-				    3'b010:  controls <= 8'b01001x00; //SW
-				    default: controls <= 8'bxxxxxxxx;
+				    3'b000:  controls <= 10'b0100011x00; //SB
+				    3'b010:  controls <= 10'b0100001x00; //SW
+				    default: controls <= 10'bxxxxxxxxxx;
+				endcase
+			7'b1100011: case(funct)
+				    3'b000:  controls <= 10'b0001000000; //BEQ
+				    default: controls <= 10'bxxxxxxxxxx;
+				endcase
+			7'b1100011: case(funct)
+				    3'b000:  controls <= 10'b0000100000; //JALR
+				    default: controls <= 10'bxxxxxxxxxx;
 				endcase
 		    7'b0110011: case(funct)
-				    3'b000:  controls <= 8'b10x00010; //ADD | SUB | MUL
-				    default: controls <= 8'bxxxxxxxx;
+				    3'b000:  controls <= 10'b10x0000010; //ADD | SUB | MUL
+				    default: controls <= 10'bxxxxxxxxxx;
 				endcase
-		    default:                 controls <= 8'bxxxxxxxx;
+		    default:                 controls <= 10'bxxxxxxxxxx;
 		endcase
 	    else
-		controls <= 8'b00000000; //NOP
+		controls <= 10'b0000000000; //NOP
 
-creg #(6) cregF(clk, dhit, {RegWriteF, MemWriteF, LoadF, ByteF, ALUSrcF, MemtoRegF}, {RegWriteD, MemWriteD, LoadD, ByteD, ALUSrcD, MemtoRegD});
-creg #(5) cregD(clk, dhit, {RegWriteD, MemWriteD, ByteD, ALUSrcD, MemtoRegD}, {RegWriteE, MemWriteE, ByteE, ALUSrcE, MemtoRegE});
-creg #(4) cregE(clk, dhit, {RegWriteE, MemWriteE, ByteE, MemtoRegE}, {RegWriteM, MemWriteM, ByteM, MemtoRegM});
+creg #(8) cregF(clk, dhit, {RegWriteF, MemWriteF, LoadF, BranchF, JumpF, ByteF, ALUSrcF, MemtoRegF}, {RegWriteD, MemWriteD, LoadD, BranchD, JumpD, ByteD, ALUSrcD, MemtoRegD});
+creg #(8) cregD(clk, dhit, {RegWriteD, MemWriteD, LoadD, BranchD, JumpD, ByteD, ALUSrcD, MemtoRegD}, {RegWriteE, MemWriteE, LoadE, BranchE, JumpE, ByteE, ALUSrcE, MemtoRegE});
+creg #(5) cregE(clk, dhit, {RegWriteE, MemWriteE, LoadE, ByteE, MemtoRegE}, {RegWriteM, MemWriteM, LoadM, ByteM, MemtoRegM});
 creg #(3) cregM(clk, dhit, {RegWriteM, ByteM, MemtoRegM}, {RegWriteW, ByteW, MemtoRegW});
 
 endmodule
